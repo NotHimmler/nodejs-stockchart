f807669acb311a91786fafdb532f3664c6c9341e
Heinrich Malan <heinrichmalan@hotmail.com>
2017-02-08 21:40:00 +1100

    fixed python bug

--- server.js
+++ server.js
@@ -24,6 +24,7 @@ io.on('connection', function(socket) {
 
         child.exec(exec, function(error, stdout, stderror){
             if(error){
+                console.log(error);
                 socket.emit('error', "Error getting stock data");
             } else {
                 if(JSON.parse(stdout) == true){
