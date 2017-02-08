04457697f81b0ad8d3f08f27069d1ed1f5b000cd
Heinrich Malan <heinrichmalan@hotmail.com>
2017-02-08 15:14:55 +1100

    added routes for getting ticker data and getting current tickers

--- app/routes.js
+++ app/routes.js
@@ -1,5 +1,18 @@
+var Tickers = require('./tickers');
+var Path = require('path');
+
 module.exports = function(app) {
     app.get('/', function(req, res) {
         res.render('home');
+    });
+
+    app.get('/get_tickers', function(req, res){
+        res.send(Tickers.tickers);
+    });
+
+    app.get('/get_data/:ticker', function(req,res){
+        var ticker = req.params.ticker;
+        var pathname = Path.join(__dirname,'../public/json',ticker+".json");
+        res.sendFile(pathname);
     })
 }
